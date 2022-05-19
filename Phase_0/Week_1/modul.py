3ba32ee3ff40688e8ae8370b3f5f96567b3450f9
Arnaz Tejakusuma <arnaz.tejakusuma@gmail.com>
2022-05-19 17:18:49 +0700

    Lecture day 3

---
+++ Phase_0/Week_1/modul.py
@@ -0,0 +1,7 @@
+import moduleTes
+
+moduleTes.greeting('Hana')
+
+from ModuleTes2 import person1
+
+print (person1['name'])
---
+++ Phase_0/Week_1/moduleTes2.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    a = 'hello, nama saya' + name
+    return a
+
+person1 = {
+ "name": "John",
+ "age": "36",
+ "country": "Norway"}
